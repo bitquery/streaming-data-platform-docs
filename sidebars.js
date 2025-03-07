c86e514a1f1f615b3ba7f7ba729230328ebdedf6
divyasshree-BQ <124589840+divyasshree-BQ@users.noreply.github.com>
2025-03-07 22:29:23 +0530

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -310,11 +310,11 @@ const sidebars = {
               "items": [
                 "examples/nft/nft-api",
                 "examples/nft/nft-blur-marketplace-api",
-                "examples/nft/nft-calls-api",
+                "examples/nft/nft-calls-apI",
                 "examples/nft/nft-collection-api",
                 "examples/nft/nft-metadata-api",
                 "examples/nft/nft-ownership-api",
-                "examples/nft/nft-trades-api",
+                "examples/nft/nft-trades-apI",
                 "examples/nft/nft-transfer-api"
               ]
             },
