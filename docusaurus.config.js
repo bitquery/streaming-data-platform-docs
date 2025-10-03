e674a17794f2f5d86fcbec74f502cff792bde112
divyasshree-BQ <124589840+divyasshree-BQ@users.noreply.github.com>
2025-10-03 16:48:23 +0530

    docusaurus title set

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require("prism-react-renderer").themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: " ",
+  title: "Bitquery",
   tagline:
     "How to query data and build applications on Bitquery blockchain data platform",
   favicon: "img/favicon.ico",
